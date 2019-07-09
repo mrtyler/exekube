ca91e82b7ea0c2e310a8ce367ced953aeea61d49
Tyler Roscoe <tyler@cryptio.net>
2019-07-08 19:35:36 -0600

    WIP binary auth policy.
    I was using this with https://github.com/gpii-ops/gpii-infra/pull/402

---
+++ modules/gke-cluster/bin_auth.tf
@@ -0,0 +1,12 @@
+resource "google_binary_authorization_policy" "policy" {
+  provider = "google-beta"
+
+  admission_whitelist_patterns {
+    name_pattern = "gcr.io/google_containers/*"
+  }
+
+  default_admission_rule {
+    evaluation_mode  = "ALWAYS_ALLOW"
+    enforcement_mode = "ENFORCED_BLOCK_AND_AUDIT_LOG"
+  }
+}
